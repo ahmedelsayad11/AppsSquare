3ed6bc7164dd58e18269590c146f9e2027b846dd
ahmed elsayad <ahmedelsayad98@gmail.com>
2022-04-25 02:42:12 +0200

    created the logic and a simple ui to represent the fakeData to produce the required output

--- src/App.js
+++ src/App.js
@@ -1,53 +1,144 @@
-import Carousal from "components/Carousal/Carousal";
-import Gallery from "components/Gallery/Gallery";
-import Modal from "components/Modal/Modal";
+import { getAllInputsApi } from "api/inputs";
+import { inputType } from "constatns/fieldsType";
 import { useEffect, useState } from "react";
-import { getAllImagesApi } from "./api/server";
 import "./App.scss";
+import InputData from "fakeData/data.json";
+import { useForm } from "react-hook-form";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [currentSlide, setCurrentSlide] = useState();
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getAllImagesApi();
-      setData(data);
+  // this function was responsible for getting the data from the endpoint
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const data = await getAllInputsApi();
+  //     setData(data);
+  //     console.log(data, "data");
+  //   }
+  //   fetchData();
+  // }, []);
+
+  const { register, handleSubmit } = useForm({});
+
+  const onSubmit = async (data) => {
+    const propertyValues = Object.values(data);
+
+    const newData = {
+      sample_id: InputData?.data?.id,
+      fields: propertyValues,
+    };
+    console.log(newData);
+  };
+
+  const defaultInputRegister = (field) => {
+    register(`${field?.id}.field_id`, { value: field?.id });
+    return null;
+  };
+  const defaultCheckBoxesRegister = (field) => {
+    register(`${field?.id}.field_id`, { value: field?.id });
+    register(`${field?.id}.options`, { value: [] });
+
+    return null;
+  };
+
+  const renderInputFields = InputData?.data?.fields?.map((field) => {
+    switch (field.type) {
+      case 4:
+        return (
+          <select
+            type={inputType[field?.type]}
+            id={field?.id}
+            name={field?.name}
+            required={field?.is_mandatory}
+            onChange={(e) => fieldChanged(e)}
+            {...register(`${field?.id}.answer`)}
+          >
+            {defaultInputRegister(field)}
+            <option value="" selected disabled hidden>
+              اختر
+            </option>
+            {field?.options?.map((option) => (
+              <>
+                <option key={option?.id} value={option?.name}>
+                  {option?.name}
+                </option>
+                {option?.has_comment && <textarea rows={15} cols={15} />}
+              </>
+            ))}
+          </select>
+        );
+      case 5:
+        return (
+          <fieldset>
+            <legend>{field?.name}</legend>
+            {field?.options?.map((option, index) => (
+              <>
+                <input
+                  type={inputType[field?.type]}
+                  id={option?.id}
+                  name={field?.id}
+                  value={option?.id}
+                  {...register(`${field?.id}.options.${index}.option_id`)}
+                />
+                {defaultCheckBoxesRegister(field)}
+                <label for={option?.name}>{option?.name}</label>
+                {option?.has_comment && (
+                  <textarea
+                    id={option?.id}
+                    name={field?.id}
+                    rows={15}
+                    cols={15}
+                    {...register(`${field?.id}.options.${index}.comment`)}
+                  />
+                )}
+                <br />
+              </>
+            ))}
+          </fieldset>
+        );
+      case 6:
+        return (
+          <fieldset>
+            <legend>{field?.name}</legend>
+            {field?.options?.map((option, index) => (
+              <>
+                {defaultInputRegister(field)}
+                <input
+                  type={inputType[field?.type]}
+                  id={option?.id}
+                  name={field?.id}
+                  value={option?.id}
+                  {...register(`${field?.id}.answer`)}
+                />
+                <label for={option?.name}>{option?.name}</label>
+                <br />
+              </>
+            ))}
+          </fieldset>
+        );
+
+      default:
+        return (
+          <div className="wrapper">
+            <label for={field?.id}>{field?.name}</label>
+            {defaultInputRegister(field)}
+            <input
+              type={inputType[field?.type]}
+              id={field?.id}
+              name={field?.name}
+              required={field?.is_mandatory}
+              {...register(`${field?.id}.answer`)}
+            />
+          </div>
+        );
     }
-    fetchData();
-  }, []);
-
-  const handleGalleryImageClick = (e) => {
-    const { id } = e?.target || {};
-    setCurrentSlide(parseInt(id));
-    setShowModal(true);
-  };
-
-  const handleNextClick = () => {
-    setCurrentSlide((p) => parseInt(p) + 1);
-  };
-  const handlePreviousClick = () => {
-    setCurrentSlide((p) => parseInt(p) - 1);
-  };
-
-  const handleCloseButtonModalClick = () => {
-    setShowModal(false);
-  };
+  });
 
   return (
     <div className="App">
-      <Modal
-        showModal={showModal}
-        handleCloseButtonModalClick={handleCloseButtonModalClick}
-      >
-        <Carousal
-          data={data}
-          currentSlide={currentSlide}
-          handleNextClick={handleNextClick}
-          handlePreviousClick={handlePreviousClick}
-        />
-      </Modal>
-      <Gallery data={data} handleGalleryImageClick={handleGalleryImageClick} />
+      <h1>appsSquare</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {renderInputFields}
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
