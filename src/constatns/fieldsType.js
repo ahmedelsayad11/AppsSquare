0fe5187cb46e784b9269bf8e00d8ae730f36ad50
ahmed elsayad <ahmedelsayad98@gmail.com>
2022-04-25 02:38:45 +0200

    created the fakeData file with the enum of input types

---
+++ src/constatns/fieldsType.js
@@ -0,0 +1,13 @@
+export const inputType = {
+  1: "text",
+  2: "text",
+  3: "number",
+  4: "select",
+  5: "checkbox",
+  6: "radio",
+  7: "file",
+  8: "image",
+  9: "date",
+  10: "date",
+  11: "email",
+};
